1873f7fbe88722a741de7bb260d7bca30bdc4b4c
Akshay Jumbade <akkyjumbade@gmail.com>
2021-11-08 18:29:21 +0530

    manage roles

--- admin/Http/Controllers/RoleController.php
+++ admin/Http/Controllers/RoleController.php
@@ -5,6 +5,8 @@ namespace Admin\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Permission;
 use App\Models\Role;
+use Illuminate\Validation\Rule;
+
 use Illuminate\Http\Request;
 use App\Models\User;
 
@@ -18,8 +20,9 @@ class RoleController extends Controller
    }
    function create() {
       $permissions = Permission::get();
+      $role = new Role();
       return view('admin::roles.create', compact(
-         'permissions'
+         'permissions', 'role'
       ));
    }
 
@@ -31,6 +34,42 @@ class RoleController extends Controller
       ]);
       try {
          $role = Role::create([
+            'title' => $req->input('title'),
+            'code' => strtolower($req->input('code', $req->input('title'))),
+            'description' => $req->input('description', $req->input('title')),
+         ]);
+         return response()->success([
+            'message' => __('Role created'),
+            'data' => $role
+         ]);
+      }catch (\Throwable $th) {
+         return response()->error([
+            'message' => $th->getMessage()
+         ]);
+      }
+   }
+   function show(Request $req, Role $role) {
+      $permissions = Permission::select([ 'title as label', 'id as value' ])->get();
+      return view('admin::roles.show', compact(
+         'permissions', 'role'
+      ));
+   }
+
+   function edit(Request $req, Role $role) {
+      $permissions = Permission::select([ 'title as label', 'id as value' ])->get();
+      return view('admin::roles.create', compact(
+         'permissions', 'role'
+      ));
+   }
+
+   function update(Request $req, Role $role) {
+      $req->validate([
+         'title' => ['required', Rule::unique('roles', 'title')->ignore($role->id)],
+         'code' => ['required', Rule::unique('roles', 'code')->ignore($role->id)],
+         'description' => ['nullable', 'max:255'],
+      ]);
+      try {
+         $role->update([
             'title' => $req->input('title'),
             'code' => strtolower($req->input('code', $req->input('title'))),
             'description' => $req->input('description', $req->input('title')),
